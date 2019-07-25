acc3195fd5a0fe9e0708d857b9a12a69d29aa85d
Allan xinhua <xinhua.allan@gmail.com>
2019-07-25 15:39:07 +0800

    login sys

--- activity/browser/demo.py
+++ activity/browser/demo.py
@@ -15,24 +15,24 @@ from pyvirtualdisplay import Display
 def getDriver():
 
     option = webdriver.ChromeOptions()
-    option.add_argument('--user-data-dir=/home/rd/.config/google-chrome') #设置成用户自己的数据目录
+    option.add_argument('--user-data-dir=../../common/config/google-chrome') #设置成用户自己的数据目录
 
     # display = Display(visible=0, size=(800, 800))  
     # display.start()
 
-    driver = webdriver.Chrome(executable_path='/home/rd/Allan/mabinogi-helper/common/tool/driver/chromedriver',chrome_options=option)     # 打开 Chrome 浏览器
-    # driver.set_window_size(150, 150)
+    driver = webdriver.Chrome(executable_path='../../common/tool/driver/chromedriver',chrome_options=option)     # 打开 Chrome 浏览器
+    driver.set_window_size(150, 150)
+    driver.implicitly_wait(10)
+
     return driver
 
 def login(driver, accounts):
 
-    print("TEST")
-
-    driver.get("https://tw.beanfun.com/index.aspx?no=defaule")
+    driver.get('https://tw.beanfun.com/index.aspx')
 
     try:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "btnLogin"))
+            EC.presence_of_element_located((By.ID, "BF_anchorLoginBtn"))
         ).click()
         
         iframe = WebDriverWait(driver, 10).until(
@@ -47,6 +47,11 @@ def login(driver, accounts):
         driver.find_element_by_id("t_AccountID").send_keys(accounts[0])
         driver.find_element_by_id("t_Password").send_keys(accounts[1])
         driver.find_element_by_id("btn_login").click()
+
+        driver.switch_to.default_content()
+
+        print('[INFO]<%s>login success'%(accounts[0]))
+
     except TimeoutException:
         print('[FAIL] <%s>login time out'%(accounts[0]))
         return False
@@ -54,24 +59,58 @@ def login(driver, accounts):
         print('[ERROR] <%s>login error:%s'%(accounts[0], str(e)))
         return False
 
+def webStart(driver, accounts, sotp):
+
+    url = 'https://tw.beanfun.com/game_zone/?service_code=600309&service_region=A2&sotp=%d'%(sotp)
+    driver.get(url)
+
     return True
+
+def logout(driver, accounts):
+    driver.get('https://tw.beanfun.com/index.aspx')
+    
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "BF_btnLogout"))
+        ).click()
+
+        iframe = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID,'fbContent'))
+        )
+        driver.switch_to.frame(iframe)
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="divConfirmButton"]/ul/li[1]/a'))
+        ).click()
+
+        print('[INFO] <%s> logout success'%(accounts))
+        
+    except TimeoutException:
+        print('[FAIL] <%s>'%(accounts))
+        return False
+    except Exception as e:
+        print('[ERROR] <%s>:%s'%(accounts, str(e)))
+        return False
+
+    return True
+
 if __name__ == "__main__":
-    driver = getDriver()
 
     with open('/home/rd/Allan/mabinogi-helper/common/config/accountsInfo.json') as f:
         accountsInfos = json.load(f)
-
+    driver = getDriver()
+    
     try:
         for item in accountsInfos:
-            driver = getDriver()
             if (login(driver, [item['user'], item['pass']]) is False):
                 driver.close()
                 driver = getDriver()
                 print("[WARN] 重新登入")
                 if (login(driver, [item['user'], item['pass']]) is False):
                     print("[ERROR] 第二次登入失敗")
-                    continue
-        input('任意鍵繼續...')
+            webStart(driver, item['user'], item['sotp'])
+            input('任意鍵繼續...')
+            logout(driver, item['user'])
             
     except Exception as e:
         print(str(e))
