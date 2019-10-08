1fbe3f469072b3828b459188144188974ff2172b
Allan xinhua <xinhua.allan@gmail.com>
2019-10-08 17:15:39 +0800

    cpu 親和

---
+++ common/tool/system/affinity.py
@@ -0,0 +1,27 @@
+import psutil
+
+def findProcessIdByName(processName):
+    '''
+    Get a list of all the PIDs of a all the running process whose name contains
+    the given string processName
+    '''
+ 
+    listOfProcessObjects = []
+ 
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+       try:
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+           # Check if process name contains the given name string.
+           if processName.lower() in pinfo['name'].lower() :
+               listOfProcessObjects.append(pinfo)
+       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+           pass
+ 
+    return listOfProcessObjects
+
+listOfProcessIds = findProcessIdByName('Client.exe')
+
+for pid in listOfProcessIds:
+    p = psutil.Process(pid['pid'])
+    p.cpu_affinity([0,1,2,3])
