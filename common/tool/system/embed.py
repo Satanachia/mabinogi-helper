dd848bdb1d9a4b11f457e3b0bc4040757b7ac4bc
Allan xinhua <xinhua.allan@gmail.com>
2019-12-12 11:14:50 +0800

    檔案整理

--- common/tool/system/embed.py
+++
@@ -1,6 +0,0 @@
-import discord
-
-embed=discord.Embed()
-embed.add_field(name="[CHANNEL1]", value="SUCCESS", inline=True)
-
-await self.bot.say(embed=embed)
--- common/tool/system/keybord.py
+++
@@ -1,6 +0,0 @@
-import win32gui
-import win32con
-
-handle = win32gui.FindWindow("Mabinogi", None) 
-print(handle)
-win32gui.ShowWindow(handle, win32con.SW_SHOW)
